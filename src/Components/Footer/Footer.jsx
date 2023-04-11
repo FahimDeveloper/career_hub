2ca5c66d39551e95512ce5d099ec7d8b0d8bfa9b
FahimDeveloper <developerfahim246@gmail.com>
2023-04-12 00:05:44 +0600

    footer section change and responsive done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,38 +3,32 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='bg-accent'>
-            <footer className="footer p-10 text-accent-content container mx-auto">
-                <div>
+            <footer className="grid grid-cols-2 gap-y-5 sm:grid-cols-4 py-10 sm:p-10 text-accent-content container mx-auto">
+                <div className='flex flex-col gap-1'>
                     <span className="footer-title">Services</span>
                     <a className="link link-hover">Branding</a>
                     <a className="link link-hover">Design</a>
                     <a className="link link-hover">Marketing</a>
                     <a className="link link-hover">Advertisement</a>
                 </div>
-                <div>
+                <div className='flex flex-col gap-1'>
                     <span className="footer-title">Company</span>
                     <a className="link link-hover">About us</a>
                     <a className="link link-hover">Contact</a>
                     <a className="link link-hover">Jobs</a>
                     <a className="link link-hover">Press kit</a>
                 </div>
-                <div>
+                <div className='flex flex-col gap-1'>
                     <span className="footer-title">Legal</span>
                     <a className="link link-hover">Terms of use</a>
                     <a className="link link-hover">Privacy policy</a>
                     <a className="link link-hover">Cookie policy</a>
                 </div>
-                <div>
-                    <span className="footer-title">Newsletter</span>
-                    <div className="form-control w-80">
-                        <label className="label">
-                            <span className="label-text text-accent-content">Enter your email address</span>
-                        </label>
-                        <div className="relative">
-                            <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" />
-                            <button className="btn btn-color absolute top-0 right-0 rounded-l-none">Subscribe</button>
-                        </div>
-                    </div>
+                <div className='flex flex-col gap-1'>
+                    <span className="footer-title">Services</span>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
                 </div>
             </footer>
         </div>
