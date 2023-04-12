8822c82fca18c051b61b929a7ee0409d7172fb26
FahimDeveloper <developerfahim246@gmail.com>
2023-04-12 11:00:42 +0600

    jobs filter based on remote and onsite task complete

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -7,7 +7,11 @@ import bgImage2 from "../../assets/All-Images/Vector.png";
 
 const AppliedJobs = () => {
     const appliedData = useLoaderData();
-    const [jobsData, setJobsData] = useState([])
+    const [jobsData, setJobsData] = useState([]);
+    const [filterData, setFilterData] = useState([]);
+    const [all, setAll] = useState(false);
+    const [remote, setRemote] = useState(false);
+    const [onSite, setOnsite] = useState(false);
     useEffect(() => {
         const storedData = getApplyData();
         const jobsArr = []
@@ -16,7 +20,27 @@ const AppliedJobs = () => {
             jobsArr.push(appliedJobs)
         }
         setJobsData(jobsArr)
-    }, [])
+    }, []);
+    useEffect(() => {
+        setFilterData(jobsData)
+    }, [jobsData, all])
+    useEffect(() => {
+        const remotesData = jobsData.filter(job => job.work_facilities[0] == "Remote")
+        setFilterData(remotesData)
+    }, [remote])
+    useEffect(() => {
+        const onSiteData = jobsData.filter(job => job.work_facilities[0] == "On-site")
+        setFilterData(onSiteData)
+    }, [onSite])
+    const handleRemoteData = () => {
+        setRemote(!remote)
+    }
+    const handleOnsiteData = () => {
+        setOnsite(!onSite)
+    }
+    const handleAllData = () => {
+        setAll(!all)
+    }
     return (
         <div>
             <div className='flex justify-center items-center h-96 bg-slate-100 relative'>
@@ -28,13 +52,13 @@ const AppliedJobs = () => {
             </div>
             <div className='container mx-auto py-20 space-y-5'>
                 <div className='text-end space-x-2'>
-                    <button className='btn btn-color'>All</button>
-                    <button className='btn btn-color'>Remote</button>
-                    <button className='btn btn-color'>On Site</button>
+                    <button onClick={handleAllData} className='btn btn-color'>All</button>
+                    <button onClick={handleRemoteData} className='btn btn-color'>Remote</button>
+                    <button onClick={handleOnsiteData} className='btn btn-color'>On Site</button>
                 </div>
                 <div className='grid grid-cols-1 gap-5 '>
                     {
-                        jobsData.map(job => <SingleAppliedJob key={job.id} {...job} />)
+                        filterData.map(job => <SingleAppliedJob key={job.id} {...job} />)
                     }
                 </div>
             </div>
