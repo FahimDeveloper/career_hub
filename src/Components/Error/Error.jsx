0a1cf55180aca29c19e17c5cc323b70d696edc59
FahimDeveloper <developerfahim246@gmail.com>
2023-04-12 11:27:34 +0600

    404 not found page create and show on router error

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Lottie from "lottie-react";
+import ErrorAnimatation from "../../assets/Error.json"
+import { useRouteError } from 'react-router-dom';
 
 const Error = () => {
+    const { error, status } = useRouteError();
     return (
-        <div>
-            error
+        <div className='w-screen h-screen flex flex-col justify-center items-center'>
+            <div className='text-center space-y-5 -mb-16'>
+                <h2 className='text-6xl font-bold'>Opps</h2>
+                <h3 className='text-xl font-medium'>{status}</h3>
+                <p className='text-lg font-medium'>{error.message}</p>
+            </div>
+            <Lottie animationData={ErrorAnimatation} loop={true} className='lg:w-1/2 xl:w-1/3' />
         </div>
     );
 };
